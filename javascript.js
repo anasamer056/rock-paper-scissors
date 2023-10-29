0d506ee23f6ea0fa926223d1c780ad156e61967a
Aanas Aamer <anasamer056@gmail.com>
2023-10-29 22:13:14 +0200

    Implement game()
    Now the game is fully functional

--- javascript.js
+++ javascript.js
@@ -6,6 +6,7 @@ function getComputerChoice() {
 }
 
 function startRound(comChoice, usrChoice) {
+
     const legend = {
         "rock" : "scissors",
         "scissors" : "paper", 
@@ -14,19 +15,63 @@ function startRound(comChoice, usrChoice) {
 
     usrChoice = usrChoice.toLowerCase();
 
+    // 0 means tie
+    // 1 means user wins
+    // -1 means computer wins 
     if (usrChoice === comChoice) {
-        return "It's a tie!";
+        return 0;
     } else if (legend[comChoice] === usrChoice) {
-        return `Computer wins! ${capitalize(comChoice)} beats ${usrChoice}`;
+        return -1;
     } else if (legend[usrChoice] === comChoice) {
-        return `You win! ${capitalize(usrChoice)} beats ${comChoice}`;
+        return 1;
     } else {
-        return "Error: Invalid input";
+        throw new Error("Invalid input");
     }
+}
+
+function game() {
+
+    let usrScore = 0;
+    let comScore = 0;
+
+    // Play 5 rounds
+    for (let i = 0; i < 5; i++) {
+
+        let comChoice = getComputerChoice();
+        let usrChoice = prompt("Rock, paper or scissors?", "rock");
+        let result;
+        try {
+            result = startRound(comChoice, usrChoice);
+        } catch (error) {
+            console.log(error);
+            return;
+        } 
+
+        if (result === 0) {
+            console.log("It's a tie!");
+        } else if (result < 0) {
+            console.log(`Computer wins! ${capitalize(comChoice)} beats ${usrChoice}`)
+            console.log(`You: ${usrScore}, Computer: ${++comScore}`);
+        } else if (result > 0) {
+            console.log(`You win! ${capitalize(usrChoice)} beats ${comChoice}`)
+            console.log(`You: ${++usrScore}, Computer: ${comScore}`);
+        }   
+    }
+
+    // Declare the final winner 
+    if (usrScore > comScore) {
+        console.log(`Congrats! You beat the computer ${usrScore} to ${comScore}`);
+    } else if (comScore > usrScore) {
+        console.log(`Uh-oh! You lost to the computer ${comScore} to ${usrScore}`);
+    } else if (usrScore === comScore) {
+        console.log(`You tied! Play again?`);
+    }
+    
 }
 
 function capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-console.log(startRound(comChoice = "paper", usrChoice = "k"))
+game();
+
